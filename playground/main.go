75721f9e9a532f4d56e375d27e7d235606e7684e
hvoostende <hvoostende@gmail.com>
2016-11-23 11:34:08 +0100

    latest

--- playground/main.go
+++ playground/main.go
@@ -1,43 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+type people []string
+
+func (p people) Len() int {
+	return len(p)
+}
+
+func (p people) Less(i, j int) bool {
+	if p[i] > p[j] {
+		return false
+	} else {
+		return true
+	}
+}
+
+func (p people) Swap(i, j int) {
+	var v string
+	v = p[i]
+	p[i] = p[j]
+	p[j] = v
+}
 
 func main() {
-	coolMap := make(map[int]string)
+	s := []string{"Zeno", "John", "Al", "Jenny"}
+	n := []int{7, 4, 8, 2, 9, 19, 12, 32, 3}
 
-	coolMap[0] = "Nice"
-	coolMap[1] = "Work"
-	coolMap[2] = "Man"
-	coolMap[3] = "!!"
+	studyGroup := people{"Zeno", "John", "Al", "Jenny"}
+	fmt.Println(studyGroup)
+	sort.Strings(studyGroup)
+	fmt.Println(studyGroup)
+	studyGroup = people{"Zeno", "John", "Al", "Jenny"}
+	fmt.Println(studyGroup)
+	sort.Sort(studyGroup)
+	fmt.Println(studyGroup)
 
-	for k, v := range coolMap {
-		fmt.Println(k, v)
-	}
+	fmt.Println(s)
+	sort.Strings(s)
+	fmt.Println(s)
 
-	var awesomeMap = map[string]bool{
-		"han":   true,
-		"joost": false,
-	}
-
-	bestMap := map[string]map[string]string{
-		"Han": map[string]string{
-			"name": "Han van Oostende",
-			"sex":  "male"},
-		"Jacqueline": map[string]string{
-			"name": "Jacqueline Longayroux",
-			"sex":  "female"},
-	}
-
-	for i := 0; i < len(coolMap); i++ {
-		fmt.Println(coolMap[i])
-	}
-
-	fmt.Println("is han cool?", awesomeMap["han"])
-
-	// delete(bestMap, "Jacqueline")
-
-	if jacqueline, ok := bestMap["Jacqueline"]; ok {
-		fmt.Printf("Jacqueline's full name is %v and she is a %v\n", jacqueline["name"], jacqueline["sex"])
-	}
+	fmt.Println(n)
+	sort.Ints(n)
+	sort.Reverse(sort.IntSlice(n))
+	fmt.Println(n)
 
 }
+
+// Use https://godoc.org/sort to sort the following:
+//
+// (1)
+// type people []string
+// studyGroup := people{"Zeno", "John", "Al", "Jenny"}
+//
+// (2)
+// s := []string{"Zeno", "John", "Al", "Jenny"}
+//
+// (3)
+// n := []int{7, 4, 8, 2, 9, 19, 12, 32, 3}
+//
+// Also sort the above in reverse order
